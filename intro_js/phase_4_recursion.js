f47d06d3cc2913fdb7e57129750c35e9381f66b4
fdiengott <freddiengott@gmail.com>
2021-04-14 16:54:32 -0400

    add recursion #exponent1, #exponent2, #fibonacci

--- intro_js/phase_4_recursion.js
+++ intro_js/phase_4_recursion.js
@@ -17,3 +17,42 @@ function sumRec(arr) {
     return arr[0] + sumRec(arr.slice(1));
 }
 
+function exponent1(base, exp) {
+    if (exp === 0) {
+        return 1; 
+    }
+
+    return base * exponent1(base, exp-1);
+}
+
+
+function exponent2(base, exp) {
+    if (exp === 0) {
+        return 1; 
+    } 
+
+    if (exp === 1) {
+        return base; 
+    }
+
+    if (exp % 2 === 0) {
+        return Math.pow(exponent2(base, exp/2), 2);
+    } else {
+        return base * Math.pow(exponent2(base, (exp - 1) / 2), 2)
+    }
+}
+
+
+function fibonacci(n) {
+    if (n === 1) {
+        return [1]; 
+    } else if (n === 2) {
+        return [1,1];
+    }
+    // debugger
+    let fibvar = fibonacci(n-1);
+
+    return fibvar.concat(fibvar[fibvar.length-1] + fibvar[fibvar.length-2]);
+}
+
+
