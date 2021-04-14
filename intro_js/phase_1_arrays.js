6dd6c43fb04e5ce5ee6a535a1cc03de8c66e94d4
fdiengott <freddiengott@gmail.com>
2021-04-14 13:50:11 -0400

    finish array methods

--- intro_js/phase_1_arrays.js
+++ intro_js/phase_1_arrays.js
@@ -9,3 +9,36 @@ Array.prototype.uniq = function() {
   return uniqArr;
 }
 
+
+Array.prototype.twoSum = function() {
+  let pairs = [];
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = i+1; j < this.length; j++) {
+
+      if (this[i] + this[j] === 0) {
+        pairs.push([i,j]);
+      }
+
+    }
+  }
+
+  return pairs;
+}
+
+
+Array.prototype.transpose = function() {
+  let transposed = [];
+
+  for (let row = 0; row < this[0].length; row++) {
+    let subarr = [];
+    for (let col = 0; col < this.length; col++) {
+      subarr.push(this[col][row]);
+
+
+    }
+    transposed.push(subarr);
+  }
+  return transposed;
+}
+
